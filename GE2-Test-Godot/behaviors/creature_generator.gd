18c90ad68fde4dad7c39da34ef3a0536cdcea831
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-23 09:50:49 +0100

    aaa

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -11,17 +11,19 @@ extends Node3D
 
 func _process(delta):
 	if Engine.is_editor_hint():		
+		pass
+
+func _ready():
+	if Engine.is_editor_hint():
 		for i in range(length):
 			var angle = start_angle + sin(deg_to_rad(i) )
 			var size = base_size * pow(multiplier, i)
 			var node = head_scene.instantiate()
 			node.name = "Tail{0}".format(i)
 			node.global_transform.origin.x = sin(angle * 1) 
-			DebugDraw3D.draw_box(Vector3(i,0,0),Quaternion.IDENTITY, Vector3(size, size, size), Color(0, 1, 0) )
+			DebugDraw3D.draw_box(Vector3(i,0,0),Quaternion.IDENTITY	, Vector3(size, size, size), Color(0, 1, 0) )
 			add_child(node)
-			print(sin(angle * i))	
-
-func _ready():
+			print(sin(angle * i))
 	if not Engine.is_editor_hint():		
 		for i in range(length):
 			var angle = start_angle + sin(deg_to_rad(i) )
