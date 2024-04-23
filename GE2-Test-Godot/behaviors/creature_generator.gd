89eafd6992027573f7b0a1ba270156eccfb9be7e
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-23 11:44:00 +0100

    pausing added

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -7,10 +7,12 @@ extends Node3D
 @export var draw_gizmos: bool = true
 @export var base_size: float = 1.0
 @export var multiplier: float= 5.0
-@export var head: Boid = null
+@export var head: PackedScene = preload("res://head.tscn")
 @export var body_scene: PackedScene = preload("res://body_part.tscn")
 
-func _process(delta):
+var head_node: Boid 
+
+func _process(_delta):
 	if Engine.is_editor_hint() and draw_gizmos:	
 		DebugDraw3D.clear_all()	
 		do_draw_gizmos()
@@ -32,8 +34,12 @@ func do_draw_gizmos():
 func _ready():
 	if Engine.is_editor_hint() and draw_gizmos:		
 		do_draw_gizmos()
-	if not Engine.is_editor_hint():		
+	if not Engine.is_editor_hint():	
+		head_node = head.instantiate()
+		$"../".add_child.call_deferred(head_node)
+		head_node.pause = true	
 		generate_worm()	
+
 
 func gen_segment(i: int) -> float:
 	var angle = start_angle + i * 2 * PI * frequency / length 
@@ -43,14 +49,18 @@ func gen_segment(i: int) -> float:
 	
 
 func generate_worm():
-	var last_position = Vector3.ZERO
+	var size = gen_segment(0)
+	var last_position: Vector3 = Vector3.ZERO
 	for i in range(length):
-		var size = gen_segment(i)
+		size = gen_segment(i)
+		last_position.x = -size/2
+		last_position.y = -size/2
+		last_position.z -= abs(size/2)
 		var body_part = body_scene.instantiate()
-		var csg : CSGBox3D = body_part.get_child(0)
-		var node_size = Vector3(size, size, size)  
-		csg.size = node_size
+		var csg : CSGBox3D = body_part.get_child(0) 
+		csg.size = Vector3(size, size, size)  
 		body_part.global_transform.origin = last_position
-		head.call_deferred("add_child", body_part)
-		last_position.z -= abs(size)
+		head_node.add_child.call_deferred(body_part)
+	
+
 
