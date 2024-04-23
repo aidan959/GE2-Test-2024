acf9bf2a8e978067c81d9b7ae38bb737e460fb22
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-23 09:22:15 +0100

    added fields

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -1,13 +1,19 @@
 @tool
 extends Node3D
 
+@export var length: int = 10
+@export var frequency: float = 1.0
+@export var base_size: float = 1.0
+@export var multiplier: float= 5.0
+@export var head_scene: Node3D
 
 func _process(delta):
 	pass		
 
 func _ready():
 	if not Engine.is_editor_hint():		
-		pass
+		
+	
 	
 
 
