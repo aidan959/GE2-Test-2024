cd552df63b621836c1caa6a9c6b253e830c7cc62
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-23 10:14:01 +0100

    added last position

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -14,27 +14,22 @@ func _process(delta):
 		DebugDraw3D.clear_all()	
 		do_draw_gizmos()
 	pass
-	
 
 
 func do_draw_gizmos():
+	var last_position = Vector3.ZERO
 	for i in range(length):
-		var angle = start_angle + sin(deg_to_rad(i) )
-		var size = base_size * pow(multiplier, i)
-		#var node = head_scene.instantiate()
-		#node.name = "Tail{0}".format(i)
-		# node.global_transform.origin.x = sin(angle * 1) 
+		var angle = start_angle + i * 2 * PI / length  
+		var size = base_size * (1 + sin(angle)) 
 		DebugDraw3D.draw_box(Vector3(i,0,0),Quaternion.IDENTITY	, Vector3(size, size, size), Color(0, 1, 0) )
-		# add_child(node)
-		# print(sin(angle * i))
 
 func _ready():
 	if Engine.is_editor_hint() and draw_gizmos:		
 		do_draw_gizmos()
 	if not Engine.is_editor_hint():		
 		for i in range(length):
-			var angle = start_angle + sin(deg_to_rad(i) )
-			var size = base_size * pow(multiplier, i)
+			var angle = start_angle + i * 2 * PI / length  
+			var size = base_size * (1 + sin(angle)) 
 			var node = head_scene.instantiate()
 			node.name = "Tail{0}".format(i)
 			node.global_transform.origin.x = sin(angle * 1) 
@@ -49,10 +44,11 @@ func _ready():
 
 func generate_worm():
 	for i in range(length):
-		var angle = start_angle + sin(deg_to_rad(i))
-		var size = base_size * pow(multiplier, i)
+		var angle = start_angle + i * 2 * PI / length  
+		var size = base_size * (1 + sin(angle)) 
 		var node = head_scene.instance()
 		node.name = "Tail{0}".format(i)
-		node.global_transform.origin.x = sin(angle * 1)
+		node.scale = Vector3(size, size, size)  
+		node.global_transform.origin.x = i  
 		add_child(node)
 
