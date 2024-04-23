73f5eab183edeb92aca224a8d6ea5dcb0dd5ea63
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2024-04-23 09:27:04 +0100

    added more variables + started writing tail code

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -3,16 +3,23 @@ extends Node3D
 
 @export var length: int = 10
 @export var frequency: float = 1.0
+@export_range (0.0, 360.0) var angle = 0.0
+
 @export var base_size: float = 1.0
 @export var multiplier: float= 5.0
-@export var head_scene: Node3D
+@export var head_scene: PackedScene = null
 
 func _process(delta):
 	pass		
 
 func _ready():
 	if not Engine.is_editor_hint():		
-		
+		pass
+
+	for i in length:
+		var node = head_scene.instantiate()
+		print(sin(angle * i))
+	
 	
 	
 
